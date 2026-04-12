416404f62817d48dfe0207d7637e4724e1d0d497
n-nakari <sanswiter8@gmail.com>
2026-04-12 18:05:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { extension_settings } from "../../../extensions.js";
 import { saveSettingsDebounced } from "../../../../script.js";
 
-const extensionName = "SillyTavern-Layout";
+// 修改了内部名称以匹配新的扩展名
+const extensionName = "Layout";
 
 // 初始化默认设置
 if (!extension_settings[extensionName]) {
@@ -19,79 +20,68 @@ if (!extension_settings[extensionName]) {
 
 const settings = extension_settings[extensionName];
 
-// 插件的UI HTML
+// 插件的UI HTML (去除了数字标号，更改了标题)
 const uiHTML = `
 <div class="inline-drawer wide100p flexFlowColumn" id="te-settings-drawer">
     <div class="inline-drawer-toggle inline-drawer-header userSettingsInnerExpandable">
-        <b><span>主题外观拓展 (Theme Editor)</span></b>
+        <b><span>界面布局</span></b>
         <div class="fa-solid fa-circle-chevron-down inline-drawer-icon down"></div>
     </div>
     <div class="inline-drawer-content" style="display: none;">
         
         <label class="checkbox_label">
             <input type="checkbox" id="te_fullscreen" />
-            <span>1. 启用全屏模式</span>
+            <span>启用全屏模式</span>
         </label>
         
         <div id="te_fs_options" style="margin-left: 20px; border-left: 2px solid var(--SmartThemeBorderColor); padding-left: 10px; display: none;">
             <div class="flex-container alignitemscenter" style="margin-bottom: 5px;">
-                <span style="font-size: 0.9em; margin-right: 10px;">1.1 底栏设置:</span>
+                <span style="font-size: 0.9em; margin-right: 10px;">底栏设置:</span>
                 <label><input type="radio" name="te_bottom_bar" value="bottom"> 置底</label>
                 <label><input type="radio" name="te_bottom_bar" value="default"> 沿用主题样式</label>
             </div>
             <label class="checkbox_label">
                 <input type="checkbox" id="te_show_bar_reply" />
-                <span>1.2 回复时显示底栏</span>
+                <span>回复时显示底栏</span>
             </label>
         </div>
         <hr>
         
         <label class="checkbox_label">
             <input type="checkbox" id="te_prevent_autofocus" />
-            <span>2. 禁止自动唤醒输入框</span>
+            <span>禁止自动唤醒输入框</span>
         </label>
         <hr>
         
         <div class="flex-container flexFlowColumn">
-            <span style="font-size: 0.9em; margin-bottom: 5px;">3. 输入时界面变化:</span>
+            <span style="font-size: 0.9em; margin-bottom: 5px;">输入时界面变化:</span>
             <div class="flex-container flexFlowColumn" style="margin-left: 20px; border-left: 2px solid var(--SmartThemeBorderColor); padding-left: 10px;">
                 <label><input type="radio" name="te_input_mode" value="none"> 无</label>
-                <label><input type="radio" name="te_input_mode" value="onlySend"> 3.1 只显示发送键</label>
-                <label><input type="radio" name="te_input_mode" value="upper"> 3.2 输入框占满上行</label>
-                <label><input type="radio" name="te_input_mode" value="lower"> 3.3 输入框占满下行</label>
+                <label><input type="radio" name="te_input_mode" value="onlySend"> 只显示发送键</label>
+                <label><input type="radio" name="te_input_mode" value="upper"> 输入框占满上行</label>
+                <label><input type="radio" name="te_input_mode" value="lower"> 输入框占满下行</label>
             </div>
         </div>
         <hr>
 
         <label class="checkbox_label">
             <input type="checkbox" id="te_collapse_qr" />
-            <span>4. 折叠QR (快速回复)</span>
+            <span>折叠QR (快速回复)</span>
         </label>
         <hr>
 
         <label class="checkbox_label">
             <input type="checkbox" id="te_collapse_preset" />
-            <span>5. 预设界面折叠</span>
+            <span>预设界面折叠</span>
         </label>
 
         <label class="checkbox_label">
             <input type="checkbox" id="te_collapse_user" />
-            <span>6. 用户设置界面折叠</span>
+            <span>用户设置界面折叠</span>
         </label>
     </div>
 </div>
 `;
-
-// 绑定SillyTavern风格的折叠面板点击事件
-function bindDrawerEvent($drawer) {
-    $drawer.find('.inline-drawer-toggle').first().on('click', function () {
-        const content = $(this).next('.inline-drawer-content');
-        const icon = $(this).find('.inline-drawer-icon');
-        content.slideToggle(200);
-        icon.toggleClass('down up');
-        icon.toggleClass('fa-circle-chevron-down fa-circle-chevron-up');
-    });
-}
 
 // 刷新 CSS class 的方法
 function updateBodyClasses() {
@@ -107,7 +97,7 @@ function updateBodyClasses() {
     $('body').toggleClass('te-collapse-qr', settings.collapseQR);
 }
 
-// 5. 预设界面折叠处理函数
+// 预设界面折叠处理函数
 function togglePresetCollapse(enable) {
     if (enable) {
         if ($('#te-preset-wrapper').length) return;
@@ -123,7 +113,6 @@ function togglePresetCollapse(enable) {
         $('#range_block_openai').before(wrapper);
         wrapper.find('.inline-drawer-content').append($('#openai_settings > div:first-child'));
         wrapper.find('.inline-drawer-content').append($('#range_block_openai'));
-        bindDrawerEvent(wrapper);
     } else {
         if (!$('#te-preset-wrapper').length) return;
         $('#te-preset-wrapper').before($('#te-preset-wrapper .inline-drawer-content > div:first-child'));
@@ -132,7 +121,7 @@ function togglePresetCollapse(enable) {
     }
 }
 
-// 6. 用户设置界面折叠处理函数
+// 用户设置界面折叠处理函数
 function toggleUserCollapse(enable) {
     if (enable) {
         // 第一部分：字体/宽度 和 Toggles 合并
@@ -150,7 +139,6 @@ function toggleUserCollapse(enable) {
             const toggleBlock = $('div[name="themeToggles"]');
             fontBlock.before(wrap1);
             wrap1.find('.inline-drawer-content').append(fontBlock).append(toggleBlock);
-            bindDrawerEvent(wrap1);
         }
 
         // 第二部分：角色处理 和 杂项 合并（但不包含 CustomCSS）
@@ -172,7 +160,6 @@ function toggleUserCollapse(enable) {
             wrap2.find('.inline-drawer-content').append(charHandling).append(miscToggles);
             // 把 CustomCSS 挪出来，放在 wrap2 下方
             wrap2.after(customCss);
-            bindDrawerEvent(wrap2);
         }
     } else {
         if ($('#te-user-wrapper-1').length) {
@@ -194,7 +181,7 @@ function toggleUserCollapse(enable) {
     }
 }
 
-// 2. 禁止自动唤醒输入框的核心拦截逻辑
+// 禁止自动唤醒输入框的核心拦截逻辑
 function setupFocusInterceptor() {
     const ta = document.getElementById('send_textarea');
     if (!ta) return;
@@ -222,8 +209,7 @@ jQuery(async () => {
     const $target = $('div[name="themeElements"] > .inline-drawer.wide100p.flexFlowColumn').first();
     $target.before(uiHTML);
 
-    const $drawer = $('#te-settings-drawer');
-    bindDrawerEvent($drawer);
+    // 注意：不再调用 bindDrawerEvent，SillyTavern的全局代码会接管 .inline-drawer-toggle 的点击事件
 
     // 还原设置状态到UI
     $('#te_fullscreen').prop('checked', settings.fullscreen);
@@ -235,8 +221,8 @@ jQuery(async () => {
     $('#te_collapse_preset').prop('checked', settings.collapsePreset);
     $('#te_collapse_user').prop('checked', settings.collapseUser);
 
-    // 显示或隐藏1.x的子选项
-    if(settings.fullscreen) $('#te_fs_options').slideDown(200);
+    // 显示或隐藏底栏设置的子选项
+    if(settings.fullscreen) $('#te_fs_options').show();
 
     // 应用初始逻辑
     updateBodyClasses();
